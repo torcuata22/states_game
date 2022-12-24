263747b42ecb5924c14d41f0ae38466b541dc1c8
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-12-24 12:30:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ guessed_states=[]
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
     guessed_states.append(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+            df = pd.DataFrame(missing_states)
+            file = df.to_csv("States_to_learn.csv")
+            
+        break
     if answer_state in all_states:
         t = turtle.Turtle()
         t.hideturtle()
@@ -34,6 +43,8 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state) #can also use state_data.state.item()
+
+
 
 
 
