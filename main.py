1d973a75a1a51269b02dd2af8bfba7af61650dc0
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-12-24 11:53:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import turtle 
+import turtle
+import pandas as pd
 
 
 
@@ -17,5 +18,29 @@ turtle.shape(image)
 # turtle.onscreenclick(get_mouse_click_coor) #onscreenclick is an event listener, waits for click to pass function that print coordinates to console
 # turtle.mainloop() #alternative way of keeping screen open
 
+
+data = pd.read_csv("50_states.csv")
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+#To do:
+# Guess to title case (use .title()?)
+
+#Check if answer_state is among the 50 states: pandas isin() function?
+
+#get ahold of all states as a list:
+all_states = data.state.to_list #transforms the data series into a list
+
+
+
+#If guess is correct, write it on the map: update screen to add text using coordinates from dataset?
+
+#Loop (keep guessing until thye get all 50 states): use while loop (until len(right_answers)=50?)
+
+#Add correct guesses to list (can't guess same state twice): append()
+
+#Keep score create scoreboard, score variable, display it and update it
+
+
+
+
 screen.exitonclick()
