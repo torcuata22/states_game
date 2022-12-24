898fe18c5fcbb5ffa3f4d6ee3676ca3f5360ae0d
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-12-24 12:06:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,23 @@ turtle.shape(image)
 # def get_mouse_click_coor(x,y):
 #     print(x,y)
 
-# turtle.onscreenclick(get_mouse_click_coor) #onscreenclick is an event listener, waits for click to pass function that print coordinates to console
-# turtle.mainloop() #alternative way of keeping screen open
+#turtle.onscreenclick(get_mouse_click_coor) #onscreenclick is an event listener, waits for click to pass function that print coordinates to console
+#turtle.mainloop() #alternative way of keeping screen open
 
 
 data = pd.read_csv("50_states.csv")
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+all_states = data["state"].to_list() 
 
-#To do:
-# Guess to title case (use .title()?)
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    print(state_data)
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(state_data.state)
 
-#Check if answer_state is among the 50 states: pandas isin() function?
-
-#get ahold of all states as a list:
-all_states = data.state.to_list #transforms the data series into a list
 
 
 
