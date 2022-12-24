f7a742edc5723877b96d178faefe980a72e1e6bf
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-12-24 12:18:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,23 @@ turtle.shape(image)
 
 
 data = pd.read_csv("50_states.csv")
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
 all_states = data["state"].to_list() 
+guessed_states=[]
 
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    print(state_data)
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
+    guessed_states.append(answer_state)
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state) #can also use state_data.state.item()
 
 
 
-
-#If guess is correct, write it on the map: update screen to add text using coordinates from dataset?
-
-#Loop (keep guessing until thye get all 50 states): use while loop (until len(right_answers)=50?)
-
-#Add correct guesses to list (can't guess same state twice): append()
-
-#Keep score create scoreboard, score variable, display it and update it
 
 
 
